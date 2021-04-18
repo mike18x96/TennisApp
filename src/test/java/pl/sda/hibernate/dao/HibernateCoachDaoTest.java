c81d7fded8bc679ffb9d11a3afdbc36cf739ba15
Michal-blip <kontakt@michalfilipiak.pl>
2021-04-18 10:34:37 +0200

    test

---
+++ src/test/java/pl/sda/hibernate/dao/HibernateCoachDaoTest.java
@@ -0,0 +1,12 @@
+package pl.sda.hibernate.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HibernateCoachDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
